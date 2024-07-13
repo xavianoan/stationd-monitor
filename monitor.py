6e94dea5c2f1984755278343735cb255fbe92637
Xavia <98714294+xavianoan@users.noreply.github.com>
2024-07-14 00:27:01 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,14 +1,14 @@
 
 import requests,subprocess,platform,time,tg
 
-command = "sudo journalctl -u tracksd -fo cat --since '1.5 min ago'"
+command = "sudo journalctl -u tracksd --no-hostname -o cat --since '1.5 min ago'"
 
 vpsname = platform.node()
 
 def monitor_stationd():
     output = subprocess.check_output(command, shell=True, text=True)
 
-    message = f"On {vpsname} Stationd log is:{output}\n"
+    message = f"On {vpsname} tracksd log is:{output}\n"
 
     url = f"https://api.telegram.org/bot{tg.TOKEN}/sendMessage?chat_id={tg.chat_id}&text={message}"
 
